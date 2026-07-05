8fd50bf894c93576d9982ddc96840771f72712a3
neharikashah <48057996+neharikashah@users.noreply.github.com>
2026-07-05 11:35:03 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # if there is a cycle return false
+
+        graph = defaultdict(list)
+
+        for first, second in prerequisites:
+            graph[second].append(first)
+
+        vis  = [0] * numCourses
+
+        def cycle(i): #cycle return true
+            if vis[i] == 1:
+                return True
+            if vis[i] == 2:
+                return False
+            vis[i] = 1
+
+            for adj in graph[i]:
+                if cycle(adj):
+                    return True
+            
+            vis[i] = 2            
+
+
+
+        for course in range(numCourses):
+            if cycle(course):
+                return False
+        
+        return True
